cd7ef8fd32ac8c6670cb7d08b765892c7007a755
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-07-09 12:58:49 +0530

    showmore functionality added

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -17,7 +17,7 @@ export default function Search() {
 
   const [listings, setListings] = useState([]);
 
-  console.log(listings);
+   const [ showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -52,9 +52,14 @@ export default function Search() {
     const fetchListings = async () => {
       try {
         setLoading(true);
+        setShowMore(false)
         const searchQuery = urlParams.toString();
         const res = await fetch(`/api/listing/get?${searchQuery}`);
         const data = await res.json();
+
+        if(data.length > 8){
+          setShowMore(true);
+        }
 
         setListings(data);
         setLoading(false);
@@ -118,6 +123,28 @@ export default function Search() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+
+      const onShowMoreClick = async ()=>{
+         const numberOfListings = listings.length;
+         const startIndex = numberOfListings;
+         const urlParams = new URLSearchParams(location.search);
+         urlParams.set('startIndex', startIndex);
+
+         const searchQuery = urlParams.toString();
+
+         const res = await fetch(`/api/listing/get?${searchQuery}`);
+
+         const data = await res.json();
+
+         if(data.length < 9){
+          setShowMore(false);
+         }
+
+         setListings([...listings, ...data])
+
+      };
+
+
 
   return (
     <div className="flex flex-col md:flex-row">
@@ -250,6 +277,11 @@ export default function Search() {
           {!loading && listings && listings.map((listing)=>(
             <ListingItem key={listing._id} listing={listing}/>
           ))}
+
+          {showMore && (
+            <button className="text-green-700 hover:underline p-7 text-center w-full" onClick={onShowMoreClick}>ShowMore</button>
+          )}
+
         </div>
       </div>
     </div>
