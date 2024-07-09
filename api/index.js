8e32a0f6cd0a3d7a946d9bb98e1c3b47c48cf393
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-07-09 16:46:40 +0530

    path was imported

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.router.js"
 import authRouter from "./routes/auth.route.js";
 import listingRouter from './routes/listing.route.js';
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config(); 
   
