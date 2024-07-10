1186dfb17e0824acceda93a49fc12587d1d7cba4
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-07-10 10:17:57 +0530

    logo to home navigation added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link>
+        <Link to={"/"}>
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Madhan</span>
             <span className="text-slate-700">Estate</span>
