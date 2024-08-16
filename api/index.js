2d7046b4e0ca80885a9c6b36c034cd2753343ab2
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-08-16 11:19:05 +0530

    job start called

--- api/index.js
+++ api/index.js
@@ -1,24 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.router.js"
+import userRouter from "./routes/user.router.js";
 import authRouter from "./routes/auth.route.js";
-import listingRouter from './routes/listing.route.js';
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import path from 'path';
+import path from "path";
+import job from "./cron/cron.js";
 
-dotenv.config(); 
-  
+dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log("Connected to MONGODB");
   })
   .catch((err) => {
-    console.log(err); 
+    console.log(err);
   });
 
-  const __dirname = path.resolve();
+job.start();
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -30,24 +33,21 @@ app.listen(3000, () => {
   console.log("Server running on 3000");
 });
 
-
-
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, `/client/dist`)));
 
-app.use(express.static(path.join(__dirname,`/client/dist`)));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
-app.get("*", (req,res)=>{
-  res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
-})
-
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Interanl Server Error";
   return res.status(statusCode).json({
-    success : false,
+    success: false,
     statusCode,
     message,
   });
